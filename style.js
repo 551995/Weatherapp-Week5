7f2ad5b05ad4835eec6840768c12d125deb14142
551995 <96127446+551995@users.noreply.github.com>
2021-12-15 18:20:41 +0100

    added cloud difference

--- style.js
+++ style.js
@@ -23,10 +23,18 @@ function formatDate(date) {
     return `${day} ${hours}:${minutes}`;
 }
 function displayWeatherCondition(response) {
-    document.querySelector("#city").innerHTML = response.data.name;
-    document.querySelector("#temperature").innerHTML = Math.round(
-        response.data.main.temp
-    );
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayTemperature);
 
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(
@@ -68,6 +76,7 @@ function convertToCelsius(event) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = 19;
 }
+
 
 // Feature #1
 let dateElement = document.querySelector("#date");
